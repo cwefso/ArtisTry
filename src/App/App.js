cab1d6b7380a1345328dd011163c00a3e2a77379
carflor <cfdeclet@gmail.com>
2020-07-21 20:55:52 -0600

    Add dependencies and removed stock content

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <section className="app">
+      <h1>ArtisTry</h1>
+    </section>
+  );
+}
+
+export default App;
