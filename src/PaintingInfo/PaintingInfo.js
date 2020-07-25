bbe000f2651ef1cbf1e7b1458331e3cd77bdc003
Joe Haefling <josephhaefling@gmail.com>
2020-07-24 20:26:29 -0600

    Show weather painting is favorited or not

--- src/PaintingInfo/PaintingInfo.js
+++ src/PaintingInfo/PaintingInfo.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './PaintingInfo.css'
 import backBtn from '../assets/back-btn.png'
-import unselctedTagBtn from '../assets/unselectedTag.png'
 import selectedTagBtn from '../assets/selectedTag.png'
+import unselectedTagBtn from '../assets/unselectedTag.png'
 import usePaintingInfo from '../Hooks/usePaintingInfo';
 import useArtistInfo from '../Hooks/useArtistInfo';
 import usePaintings from '../Hooks/usePaintings'
@@ -14,18 +14,19 @@ function PaintingInfo(props) {
   const {userFavs} = props.favorites
   const {title, image, completitionYear, artistName, artistId, artistUrl, height, width} = props.paintingInfo
   const data = usePaintingInfo(title, artistName)
+  const tagBtn = isFavorite ? selectedTagBtn : unselectedTagBtn
 
 useEffect(() => {
   if(userFavs) {
     const isPaintingAFav = userFavs.find(favorite => favorite.artistContentId === artistContentId)
-    isPaintingAFav && setIsFavorite(true)
+    isPaintingAFav && setIsFavorite(true) 
   }
 }, []) 
 
   // console.log(data.artistUrl)
   // const artistPaintings = usePaintings(`http://www.wikiart.org/en/App/Painting/PaintingsByArtist?artistUrl=${data.artistURL}&json=2`)
 
-
+console.log(tagBtn);
   return(
     <section className="painting-page">
       <section className="painting-nav">
@@ -33,7 +34,7 @@ useEffect(() => {
           <img src={backBtn} alt='back-btn' className='back-btn' />
         </Link>
         <h1 className="painting-title">{title}</h1>
-        {/* <img src={tagBtn} alt='save-btn' className='save-btn' /> */}
+        <img src={tagBtn} alt='save-btn' className='save-btn' />
       </section>
       <section className="painting-data-container">
         <section className="painting-box">
