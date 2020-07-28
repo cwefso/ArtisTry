ee43fd07772d5f8dda961090cccc35e33224e681
Charles Wefso <cwefso@gmail.com>
2020-07-28 15:57:36 -0600

    fixes typo

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,7 +8,7 @@
   }; 
 
   export const getFavorites = () => {
-    return fetch('http://artistry-turing-app.herokuapp.com/api/v1/favorites')
+    return fetch('https://artistry-turing-app.herokuapp.com/api/v1/favorites')
       .then(res => res.json())
       .then(data => data.favorites)
   }; 
